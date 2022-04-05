de02ee8389890587d44aa7790b521e88df75da6c
Gero <gersonhoa@gmail.com>
2022-04-05 17:12:28 -0300

    fix: querry fixement

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,1 @@
-SELECT * FROM purchase_orders WHERE created_By > 3 ORDER BY created_by;
+SELECT * FROM purchase_orders WHERE created_By > 3 ORDER BY created_by DESC;
--- desafio17.sql
+++ desafio17.sql
@@ -1,1 +1,1 @@
-SELECT id, supplier_id FROM purchase_orders WHERE supplier_id = 1 OR 3 OR 5 OR 7;
+SELECT id, supplier_id FROM purchase_orders WHERE supplier_id = 1 OR supplier_id = 3 OR supplier_id = 5 OR supplier_id = 7;
